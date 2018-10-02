5396dfcac7ccacc457f95303d6ead45735b8f2f5
inc0gnito <yashanand155@gmail.com>
2018-10-02 23:30:32 +0530

    test4.js
    reverse_cipher

--- test4.js
+++ test4.js
@@ -1,1 +1,10 @@
-adsad
+#Reverse Cipher
+#"hello !" =========>"! olleh"
+#message = 'This is a secret Msg!'
+message = input('ENTER THE MESSAGE: ')
+translated = ''
+s = len(message) - 1 #len() to find the string length
+while s >=0:
+        translated = translated + message[s]
+        s = s - 1
+print(translated)
